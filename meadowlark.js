5eb078edb7a50b71806b3961dd4eae4a2c2e085b
William Friesen <wfriesen@gmail.com>
2015-12-24 15:24:41 +1100

    Added newsletter sign up and thank you page

--- meadowlark.js
+++ meadowlark.js
@@ -94,6 +94,22 @@ app.get('/data/nursery-rhyme', function(req, res){
   });
 });
 
+app.use(require('body-parser')());
+
+app.get('/newsletter', function(req, res){
+  res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+app.get('/thank-you', function(req, res){
+  res.render('thank-you');
+});
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res){
   res.status(404);
