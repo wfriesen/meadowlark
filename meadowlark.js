5f2c3a5b1225e7acadadc512e57954c39acedc7f
William Friesen <wfriesen@gmail.com>
2015-12-27 17:52:49 +1100

    Added Sessions and flash message to newsletter signup

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,7 @@ var fortune = require('./lib/fortune.js');
 var express = require('express');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials.js');
 
 var app = express();
 
@@ -26,6 +27,20 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' &&
     req.query.test === '1';
+  next();
+});
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret,
+}));
+
+app.use(function(req, res, next){
+  // if there's a flash message, transfer it to the context, then clear it
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
   next();
 });
 
@@ -131,6 +146,48 @@ app.post('/process', function(req, res){
 });
 app.get('/thank-you', function(req, res){
   res.render('thank-you');
+});
+
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+  cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+  var name = req.body.name || '', email = req.body.email || '';
+  // input validation
+  if (!email.match(VALID_EMAIL_REGEX)) {
+    if (req.xhr) return res.json({error: 'Invalid name/email address.'});
+    req.session.flash = {
+      type: 'danger',
+      intro: 'Validation error!',
+      message: 'The email address your entered was not valid.',
+    };
+    return res.redirect(303, '/newsletter/archive');
+  }
+
+  new NewsletterSignup({name: name, email: email}).save(function(err){
+    if (err) {
+      if (req.xhr) return res.json({error: 'Database error.' });
+      req.session.flash = {
+        type: 'danger',
+        intro: 'Database error!',
+        message: 'There was a database error; please try again later.',
+      };
+      return res.redirect(303, '/newsletter/archive');
+    }
+    if (req.xhr) return res.json({success: true});
+    req.session.flash = {
+      type: 'success',
+      intro: 'Thank you!',
+      message: 'You have now been signed up for the newsletter.',
+    };
+    return res.redirect(303, '/newsletter/archive');
+  });
 });
 
 app.use('/upload', function(req, res, next){
